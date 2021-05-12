97be0d0b709b77f975fff58b733f76769a5b0ebe
nakodil <57211991+nakodil@users.noreply.github.com>
2021-05-12 17:33:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from interface import *
+
+app = QtWidgets.QApplication(sys.argv)
+MainWindow = QtWidgets.QMainWindow()
+ui = Ui_MainWindow()
+ui.setupUi(MainWindow)
+MainWindow.show()
+
+
+
+sys.exit(app.exec_())
